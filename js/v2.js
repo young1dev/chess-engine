4a330fd6e307a9fc99d85d15fdd9ca9dd937b7ec
Isaac <fatokunoi@gmail.com>
2026-06-07 23:31:08 +0100

    Updated v2 code

--- js/v2.js
+++ js/v2.js
@@ -1,117 +1,108 @@
 const pieceValue = {
   Pawn: 1,
-  Rook: 5,
   Knight: 3,
-  Queen: 5,
   Bishop: 3,
+  Rook: 5,
+  Queen: 9,
 };
 
+const aiColor = true ? "black" : "white";
+
 function minmax(boardState, depth, maximizingPlayer) {
-  const newBoard = clonedBoard(boardState);
-
-  if (depth === 0 || isCheckmate) {
+  const color = maximizingPlayer ? "white" : "black";
+  if (depth === 0) {
     //const posEval = posEval(boardState)
-    console.log(posEval(boardState));
-    return posEval(newBoard);
+    // console.log(posEval(boardState));
+    return posEval(boardState);
   }
+
   if (maximizingPlayer) {
-    const maxEval = -Infinity;
-    console.log(maxEval);
-    const allPossibleMoves = getAllMovesForEachPiece("black"); //later opponent will be there
+    let maxEval = -Infinity;
+    let bestMove = -Infinity; 
+    let bestPosition = null;
+    const movesList = getAllMovesForEachPiece(boardState, color);
 
-    allPossibleMoves.forEach((moves) => {
-      console.log(moves);
-      newBoard[moves.toRow][moves.toCol] =
-        newBoard[moves.fromRow][moves.fromCol];
+    for (const moves of movesList) {
+      // console.log(movesList);
+      // console.log("FROM PIECE:", boardState[moves.fromRow][moves.fromCol]);
+      const tempBoard = clonedBoard(boardState);
+      tempBoard[moves.toRow][moves.toCol] =
+        tempBoard[moves.fromRow][moves.fromCol];
 
-      const eval = minmax(newBoard, depth - 1, false);
-      maxEval = max(maxEval, eval);
-      console.log(maxEval);
-    });
+      tempBoard[moves.fromRow][moves.fromCol] = null;
+      const eval = minmax(tempBoard, depth - 1, !maximizingPlayer);
+      maxEval = Math.max(maxEval, eval);
+      if (maxEval > bestMove) {
+       bestPosition = moves;
+        bestMove = maxEval;
+      }
+    }
     return maxEval;
   } else {
-    //minimizing player
-    const minEval = +Infinity;
-    console.log(minEval);
-    const allPossibleMoves = getAllMovesForEachPiece("white"); //later opponent will be there
-    allPossibleMoves.forEach((moves) => {
-      newBoard[moves.toRow][moves.toCol] =
-        newBoard[moves.fromRow][moves.fromCol];
+    let minEval = Infinity;
+    let bestPosition = Infinity;
+    let bestMove = null;
+    const movesList = getAllMovesForEachPiece(boardState, color);
 
-      const eval = minmax(newBoard, depth - 1, false);
-      minEval = min(minEval, eval);
-      console.log(minEval);
-    });
+    for (const moves of movesList) {
+      const tempBoard = clonedBoard(boardState);
+      tempBoard[moves.toRow][moves.toCol] =
+        tempBoard[moves.fromRow][moves.fromCol];
+
+      tempBoard[moves.fromRow][moves.fromCol] = null;
+      const eval = minmax(tempBoard, depth - 1, !maximizingPlayer);
+      minEval = Math.min(minEval, eval);
+      //  if (minEval < bestMove) {
+      //   bestPosition = moves;
+      //   console.log(bestPosition);
+      //   bestMove = maxEval;
+      //   console.log(bestMove)
+      // }
+    }
     return minEval;
   }
 }
 
-minmax(boardState, 4, false);
+minmax(boardState, 1, true);
 
 function posEval(clonedBoard) {
-  let whiteArr = [];
-  let blackArr = [];
+  let score = 0;
+
   for (let fromRow = 0; fromRow < 8; fromRow++) {
     for (let fromCol = 0; fromCol < 8; fromCol++) {
-      const piece = boardState[fromRow][fromCol];
+      const piece = clonedBoard[fromRow][fromCol];
       if (!piece || piece.type === "King") continue;
 
-      //   console.log(piece.type);
-
-      //   console.log(
-      //     `${whiteArr} - ${piece.type}: ${pieceValue[piece.type]}, ${blackArr} - ${piece.type}: ${pieceValue[piece.type]}`,
-      //   );
-
       if (piece.color === "white") {
-        whiteArr.push(pieceValue[piece.type]);
-      }
-      if (piece.color === "black") {
-        blackArr.push(pieceValue[piece.type]);
+        score += pieceValue[piece.type];
+      } else {
+        score -= pieceValue[piece.type];
       }
     }
   }
-
-  //To get the diff between them
-  let wTotal = 0;
-  let bTotal = 0;
-  //console.log(whiteArr);
-  let prev = 0;
-  for (let i = 0; i < whiteArr.length; i++) {
-    prev = prev + whiteArr[i];
-    wTotal = prev;
-  }
-  prev = 0;
-  for (let i = 0; i < blackArr.length; i++) {
-    prev = prev + whiteArr[i];
-    bTotal = prev;
-  }
-  //   console.log(`White: ${wTotal} | Black: ${bTotal}`);
-  const posDiff = bTotal - wTotal;
-  //   console.log(posDiff)
-  return posDiff;
+  return score;
 }
+// console.log(posEval(boardState));
 
-function getAllMovesForEachPiece(color) {
-  const allPossibleMoves = [];
-  const newBoard = clonedBoard(boardState);
+function getAllMovesForEachPiece(boardState, color) {
+  const movesList = [];
   for (let fromRow = 0; fromRow < 8; fromRow++) {
     for (let fromCol = 0; fromCol < 8; fromCol++) {
-      const piece = newBoard[fromRow][fromCol];
+      const piece = boardState[fromRow][fromCol];
       if (!piece || !piece.color) continue;
 
       for (let toRow = 0; toRow < 8; toRow++) {
         for (let toCol = 0; toCol < 8; toCol++) {
-          if (newBoard[fromRow][fromCol].color !== color) continue;
           const legalMoves = isLegalMove(
             toRow,
             toCol,
             fromRow,
             fromCol,
-            newBoard,
+            boardState,
             color,
           );
           if (legalMoves) {
-            allPossibleMoves.push({ fromRow, fromCol, toRow, toCol });
+            movesList.push({ fromRow, fromCol, toRow, toCol });
           }
 
           //To check piece ratings/value
@@ -120,6 +111,6 @@ function getAllMovesForEachPiece(color) {
       }
     }
   }
-  return allPossibleMoves;
+  return movesList;
 }
-// console.log(getAllMovesForEachPiece("black"));
+// console.log(getAllMovesForEachPiece(boardState, "white"));
